99d2bb5e12b9cdd65ebc6a250205c0adcd47b9f9
Collin McCloskey <collin@transportatx.com>
2024-04-29 15:44:12 -0600

    Modify server for deployment testing

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Respond to all requests with a simple message
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello from Aria Server!');
+});
+
+// You can also catch all other routes to ensure no 404 errors during tests
+app.get('*', (req, res) => {
+    res.send('Hello from Aria Server - Catch All Route!');
 });
 
 const PORT = process.env.PORT || 3000;
